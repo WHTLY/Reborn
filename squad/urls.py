324a4fa68fd8a27232453f3bd6a0042124d096d4
Artem D. Vasilev <tiberzann@yandex.ru>
2014-07-01 17:18:43 +0400

    alpha 1

--- squad/urls.py
+++ squad/urls.py
@@ -3,6 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-
+    url(r'^logo/(?P<user>\w{0,50})$', 'squad.views.retxml'),
     url(r'^$', 'squad.views.overview'),
 )+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- squad/views.py
+++ squad/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from squad.models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 
 
 def overview(request):
@@ -8,3 +10,10 @@ def overview(request):
 
     return render(request, 'squad_overview.html', {'ranks': ranks, 'members': members})
 
+
+def retxml(request, user):
+    try:
+        member = Member.objects.get(nickname=user)
+        return render(request, 'squad.xml', {'member': member})
+    except ObjectDoesNotExist:
+        return HttpResponse(None)
