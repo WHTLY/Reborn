8ba644c8ea5ec9e9b570bd66afeb2ab8c556e23f
Artem D. Vasilev <tiberzann@yandex.ru>
2014-07-02 20:54:23 +0400

    fixing xml

--- squad/urls.py
+++ squad/urls.py
@@ -5,6 +5,8 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     url(r'^logo/(?P<user>\w{0,50})$', 'squad.views.retxml'),
-    url(r'^logo/.*paa$', 'squad.views.retlogo'),
+    url(r'^logo/.*paa$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
     url(r'^$', 'squad.views.overview'),
 )+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
